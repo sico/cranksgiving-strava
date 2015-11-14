e1bc06a3c9f1b21c4bb61c060027ed02cb0e03e3
sico <sico@cozy.co>
2015-11-13 17:39:25 -0800

    Make it look fancier

--- config.ru
+++ config.ru
@@ -1,2 +1,10 @@
+require 'rubygems'
+require 'bundler'
+require 'sass/plugin/rack'
 require './strava'
+
+# use scss for stylesheets
+Sass::Plugin.options[:style] = :compressed
+use Sass::Plugin::Rack
+
 run Sinatra::Application
--- strava.rb
+++ strava.rb
@@ -7,9 +7,6 @@ client = OAuth2::Client.new(ENV['STRAVA_APP_ID'], ENV['STRAVA_SECRET'], :site =>
 get '/' do
 
 	@auth_url = client.auth_code.authorize_url(:redirect_uri => ENV['BASE_URL'] + 'callback')
-	# => "https://example.org/oauth/authorization?response_type=code&client_id=client_id&redirect_uri=http://localhost:8080/oauth2/callback"
-
-	# response.class.name
 
   erb :index
 end
@@ -17,7 +14,6 @@ end
 get '/callback' do
 	code = params['code']
 	@token = client.auth_code.get_token(code, :redirect_uri => ENV['BASE_URL'] + '/oauth2/callback')
-	# response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
 
 	erb :callback
 end
